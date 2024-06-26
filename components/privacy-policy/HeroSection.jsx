ebdadccb3c8efa25cc86236a618ffcd813d69486
arsahak <arsahak99@gmail.com>
2024-06-26 13:08:35 +0600

    update

--- components/privacy-policy/HeroSection.jsx
+++ components/privacy-policy/HeroSection.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import SectionLayout from "../shared/SectionLayout";
-import MotionEffect from "../motion/MotionEffect";
 import PageHeroSection from "../shared/PageHeroSection";
 
 const HeroSection = () => {
