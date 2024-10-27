bb27b6ef2ea80a47ea723850d1b11f8a44e1830c
arsahak <arsahak99@gmail.com>
2024-10-27 14:56:41 +0600

    jayed meta tag update

--- app/career/page.jsx
+++ app/career/page.jsx
@@ -6,7 +6,7 @@ import React from "react";
 export const metadata = {
   title: "Career - Attorney Hardam Tripathi, Esq.",
   description:
-    "Stay informed on the latest immigration news, legal updates, and valuable resources with TripLaw's immigration blog",
+    "Stay informed on the latest immigration news, legal updates, and valuable resources with TripLaw's immigration blog ",
 };
 
 const page = () => {
