6d8c9029c204cf4c9755fb93d07a981b37639388
arsahak <arsahak99@gmail.com>
2025-03-09 11:38:57 +0600

    Remove Vercel analytics and speed insights from package.json and layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,8 @@
+import Footer from "@/components/Footer";
+import MainNavbar from "@/components/MainNavbar";
 import { Mulish } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import MainNavbar from "@/components/MainNavbar";
-import Footer from "@/components/Footer";
 
 const mulish = Mulish({ subsets: ["latin"] });
 
@@ -20,8 +18,6 @@ export default function RootLayout({ children }) {
           <div className="overflow-x-hidden text-black bg-white">
             <MainNavbar />
             {children}
-            <Analytics />
-            <SpeedInsights />
             <Footer />
           </div>
         </Providers>
