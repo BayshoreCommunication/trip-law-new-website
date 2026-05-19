1ca6b7d9dce72a2f904aad6a4b80d4494fc201c7
sajolbd <shamim15-9760@diu.edu.bd>
2026-05-19 14:49:43 +0600

    image change from 1st blog

--- components/static-blogs/blogs/visaOverstayMeta.js
+++ components/static-blogs/blogs/visaOverstayMeta.js
@@ -5,7 +5,7 @@ export const VISA_OVERSTAY_DESCRIPTION =
   "Find out what happens after a U.S. visa overstay and explore ways to fix it, including waivers and family based options.";
 
 export const VISA_OVERSTAY_FEATURE_IMAGE =
-  "/assets/static-blogs/ us-visa-overstay-consequences-and-solutions.webp";
+  "/assets/static-blogs/us-visa-overstay-consequences-and-solutions.webp";
 
 export const VISA_OVERSTAY_FEATURE_IMAGE_ALT =
   "U.S. visa overstay legal options";
